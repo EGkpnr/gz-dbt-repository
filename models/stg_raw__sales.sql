d3282fa769fe4825f9cd93df50ce0099ab9c6252
Emrah Gökpınar <e.gkpnr@gmail.com>
2025-10-09 16:44:43 +0000

    stagging created

---
+++ models/stg_raw__sales.sql
@@ -0,0 +1,7 @@
+-- models/staging/stg_raw__sales.sql
+with
+    source as (select * from {{ source("raw", "sales") }}),
+    renamed as (select date_date, orders_id, pdt_id, revenue, quantity from source)
+select *
+from renamed
+;
