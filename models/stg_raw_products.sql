d43c8a9dfaeeaaf2eb1c695a3916c4322603ce80
Emrah Gökpınar <e.gkpnr@gmail.com>
2025-10-12 18:35:27 +0000

    devam

--- models/stg_raw_products.sql
+++ models/stg_raw_products.sql
@@ -1,7 +1,7 @@
 with
     source as (select * from {{ source("raw", "products") }}),
     renamed as (
-        select products_id, cast(purchse_price as float64) as purchase_price from source
+        select products_id, cast(purchSE_PRICE as float64) as purchase_price from source
     )
 select *
 from renamed
