6c973afd028b183ad3c8f35260c2b621e7afd74d
Emrah Gökpınar <e.gkpnr@gmail.com>
2025-10-12 20:13:24 +0000

    123

--- models/intermediate/int_sales_margin.sql
+++ models/intermediate/int_sales_margin.sql
@@ -1,10 +1,10 @@
 select
-    s.pdt_id,
+    s.products_id,
     s.date_date,
     s.orders_id,
     s.revenue,
     s.quantity,
-    p.purchSE_PRICE as purchase_price,
+    p.purchase_price,
     round(s.quantity * p.purchase_price, 2) as purchase_cost,
     round(s.revenue - s.quantity * p.purchase_price, 2) as margin
 from {{ ref("stg_raw__sales") }} s
