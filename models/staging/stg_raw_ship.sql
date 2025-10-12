85d055a3fd5ae4f386d19c9360a7e47a773af081
Emrah Gökpınar <e.gkpnr@gmail.com>
2025-10-12 22:31:28 +0000

    123

--- models/staging/stg_raw_ship.sql
+++ models/staging/stg_raw_ship.sql
@@ -1,5 +1,5 @@
 with
-    source as (select * from {{ source("raw", "ship") }}),
+    source as (select * from {{ source("raw", "raw_gz_ship") }}),
     renamed as (
         select
             orders_id,
@@ -7,7 +7,11 @@ with
             logcost as log_cost,
             cast(ship_cost as int64) as ship_cost
         from source
+        where
+            orders_id is not null
+            or shipping_fee is not null
+            or logcost is not null
+            or ship_cost is not null
     )
-
 select *
 from renamed
