da483bb56fd6d4041234ab183ab9b06780df106e
Emrah Gökpınar <e.gkpnr@gmail.com>
2025-10-12 20:08:43 +0000

    asd

--- models/intermediate/int_sales_margin.sql
+++ models/intermediate/int_sales_margin.sql
@@ -4,7 +4,7 @@ select
     s.orders_id,
     s.revenue,
     s.quantity,
-    p.purchSE_PRICE as p.purchase_price,
+    p.purchSE_PRICE as purchase_price,
     round(s.quantity * p.purchase_price, 2) as purchase_cost,
     round(s.revenue - s.quantity * p.purchase_price, 2) as margin
 from {{ ref("stg_raw__sales") }} s
--- models/staging/stg_raw_products.sql
+++ models/staging/stg_raw_products.sql
@@ -1,7 +1,8 @@
 with
     source as (select * from {{ source("raw", "products") }}),
     renamed as (
-        select products_id, cast(purchSE_PRICE as float64) as purchase_price from source
+        select products_id, cast(purchSE_PRICE as float64) as purchase_price
+        from source
     )
 select *
 from renamed
